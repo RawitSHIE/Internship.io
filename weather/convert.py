1f87f680dd0353f77fb96da1fbca8d7cf90846a7
Rawit Lohakhachornphan <Rawitgun@gmail.com>
2019-01-29 21:24:24 +0700

    weather xml->json

---
+++ weather/convert.py
@@ -0,0 +1,17 @@
+"""
+weather file 
+xml -> json
+ref : github/martinblech/xmltodict
+"""
+
+import xmltodict
+import json
+import sys
+def main():
+    """single function"""
+    xml = open(sys.argv[1], "r").read()
+    jsn = open("weather.json", "w")
+    jsn.write(json.dumps(xmltodict.parse(xml, attr_prefix='')['current'], indent=2))
+    jsn.close
+
+main()
